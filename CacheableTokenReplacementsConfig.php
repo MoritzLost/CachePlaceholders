6706998345defdd6066ec0379cbc01df38871e24
Moritz L'Hoest <info@herebedragons.world>
2020-05-06 11:35:12 +0200

    Display token list and validation in module config

--- CacheableTokenReplacementsConfig.php
+++ CacheableTokenReplacementsConfig.php
@@ -38,9 +38,52 @@ class CacheableTokenReplacementsConfig extends ModuleConfig
         $PageRenderHookFrontendOnly->columnWidth = 50;
         $PageRenderHookFrontendOnly->collapsed = Inputfield::collapsedNever;
 
+        $module = $this->modules->get('CacheableTokenReplacements');
+        $currentTokens = $module->getTokens();
+        $tokenListMarkup = [];
+        foreach ($currentTokens as $name => $token) {
+            $nameIsValid = preg_match(
+                sprintf('/^%s$/', CacheableTokenReplacements::TOKEN_NAME_REGEX)
+                , $name
+            ) === 1;
+            $callbackIsValid = (is_array($token) || $token instanceof \ArrayAccess)
+                && isset($token[CacheableTokenReplacements::TOKEN_KEY_CALLBACK])
+                && is_callable($token[CacheableTokenReplacements::TOKEN_KEY_CALLBACK]);
+            $errors = [
+                !$nameIsValid ? $this->_('The token name contains invalid characters.') : null,
+                !$callbackIsValid ? $this->_('The token definition does not include a valid callback.') : null,
+            ];
+            $hasErrors = array_filter($errors);
+            $checkMarkup = $hasErrors
+                ? implode("\n", array_map(function ($line) {
+                        return sprintf('<p><i class="fa fa-fw fa-times-circle"></i> %s</p>', $line);
+                    }, array_filter($errors)))
+                : sprintf('<i class="fa fa-fw fa-check"></i> %s', $this->_('This token is valid.'));
+            $tokenListMarkup[] = sprintf(
+                '<div style="margin-bottom:.5rem;">
+                    <h3 style="margin-bottom:.25rem;">%1$s</h3>
+                    <div class="%2$s">%3$s</div>
+                </div>',
+                $name,
+                $hasErrors ? 'uk-alert-danger' : 'uk-alert-success',
+                $checkMarkup
+            );
+        }
+        $CurrentTokenDisplay = $this->modules->get('InputfieldMarkup');
+        $CurrentTokenDisplay->label = $this->_('Token list');
+        $CurrentTokenDisplay->description = $this->_('This list will show you all the tokens that currently exist. Use this to check if your custom tokens are being registered correctly. You will also see warnings if a token has an invalid name or is missing a valid callback.');
+        $CurrentTokenDisplay->columnWidth = 50;
+        $CurrentTokenDisplay->collapsed = Inputfield::collapsedNever;
+        $CurrentTokenDisplay->value = sprintf('%s', implode("\n", $tokenListMarkup));
+
+        $UsageInstructions = $this->modules->get('InputfieldMarkup');
+        $UsageInstructions->label = $this->_('Usage instructions');
+        $UsageInstructions->value = '@TODO: Write instructions.';
+        $UsageInstructions->columnWidth = 50;
+        $UsageInstructions->collapsed = Inputfield::collapsedNever;
+
         $DelimiterSettings = $this->modules->get('InputfieldFieldset');
         $DelimiterSettings->label = $this->_('Token format & delimiters');
-        // @TODO: example token with current delimiters
         $DelimiterSettings->collapsed = Inputfield::collapsedYes;
 
         $delimiterFields = [
@@ -55,12 +98,18 @@ class CacheableTokenReplacementsConfig extends ModuleConfig
             $DelimiterField = $this->modules->get('InputfieldText');
             $DelimiterField->name = $name;
             $DelimiterField->label = $label;
-            $DelimiterField->columnWidth = 20;
+            $DelimiterField->columnWidth = 33;
             $DelimiterField->collapsed = Inputfield::collapsedNever;
             $DelimiterField->notes = sprintf($this->_('Default: `%s`'), $defaults[$name]);
+            $DelimiterField->required = true;
+            $DelimiterField->requiredAttr = true;
+            $DelimiterField->minlength = 1;
+            $DelimiterField->maxlength = 5;
             $DelimiterSettings->add($DelimiterField);
         }
 
+        $inputfields->add($UsageInstructions);
+        $inputfields->add($CurrentTokenDisplay);
         $inputfields->add($PageRenderHookActive);
         $inputfields->add($PageRenderHookFrontendOnly);
         $inputfields->add($DelimiterSettings);
